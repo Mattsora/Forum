76846f11547dfb969767337bd6cb83922fa4b74d
PieterV <pieterverschueren@outlook.com>
2019-09-03 13:47:32 +0200

    Made login better

--- login.php
+++ login.php
@@ -1,17 +1,9 @@
 <?php
 require 'config.php';
+require 'header.php';
 ?>
 
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+<div class="masthead">
 <form method="post" action="loginController.php">
     <input type="hidden" name="type" value="login">
     <div class="form-group">
@@ -25,6 +17,7 @@ require 'config.php';
     </div>
     <input type="submit" value="login">
 </form>
-</body>
-</html>
-
+</div>
+<?php
+require 'footer.php';
+?>
